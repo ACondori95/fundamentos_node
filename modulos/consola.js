742c7009275cdfe848824e02128c8da5297902c0
Alvaro Condori <con21033@byui.edu>
2023-08-22 19:39:13 -0300

    Entender los módulos del core

---
+++ modulos/consola.js
@@ -0,0 +1,51 @@
+// console.log("Algo");
+// console.info("Algo");
+// console.error("Algo");
+// console.warn("Algo");
+
+// var tabla = [
+//   {
+//     a: 1,
+//     b: "Z",
+//   },
+//   {
+//     a: 2,
+//     b: "Otra",
+//   },
+// ];
+// console.log(tabla);
+// console.table(tabla);
+
+// console.group("Conversación");
+// console.log("Hola");
+// console.group("bla");
+// console.log("Blablabla");
+// console.log("Blablabla");
+// console.log("Blablabla");
+// console.groupEnd("bla");
+// console.log("Adios");
+// console.groupEnd("Conversación");
+// console.log("otras cosas de otra función");
+
+// function funcion1() {
+//   console.group("funcion 1");
+//   console.log("Esto es de la funcion 1");
+//   console.log("Esto tambien");
+//   console.log("Esto tambien");
+//   funcion2();
+//   console.log("Hemos vuelto a la 1");
+//   console.groupEnd("funcion 1");
+// }
+// function funcion2() {
+//   console.group("funcion 2");
+//   console.log("ahora estamos en la funcion 2");
+//   console.groupEnd("funcion 2");
+// }
+// console.log("empezamos");
+// funcion1();
+
+console.count("veces");
+console.count("veces");
+console.count("veces");
+console.countReset("veces");
+console.count("veces");
